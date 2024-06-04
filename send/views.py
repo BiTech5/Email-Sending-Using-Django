bbdd3fc55df889ac0e647424023632b11d4195c7
BiTech5 <bikikc7@gmail.com>
2024-06-04 10:51:50 +0545

    getting the data from form

--- send/views.py
+++ send/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    if request.method=="POST":
+        title=str(request.POST.get('title'))
+        fir_name=str(request.POST.get('first'))
+        las_name=str(request.POST.get('last'))
+        body=str(request.POST.get('body'))
+        print(f'{title}\n{fir_name} {las_name}\n\t{body}')
     return render(request,'main.html')
